d7daa7b28de468173ac328d8074f3de04a605e79
h3sun <haihaosun15@gmail.com>
2024-08-24 00:29:51 -0700

    add defense in example.py

--- GNNIP/core_algo/Defense.py
+++ GNNIP/core_algo/Defense.py
@@ -1,4 +1,4 @@
-from GNNIP.datasets import *
+from gnnip.datasets import *
 import networkx as nx
 import numpy as np
 import torch as th
@@ -17,7 +17,7 @@ from torch_geometric.data import Data
 from torch_geometric.nn import SAGEConv
 from torch_geometric.utils import erdos_renyi_graph
 from torch_geometric.loader import NeighborSampler
-from GNNIP.core_algo import *
+from gnnip.core_algo import *
 from dgl.data import CoraGraphDataset
 from dgl.dataloading import NeighborSampler
 from dgl.nn import SAGEConv
@@ -27,8 +27,9 @@ from tqdm import tqdm
 import os
 # from dgl.dataloading.dataloader import enable_cpu_affinity
 
+
 class graph_to_dataset:
-    def __init__(self, graph,attack_node_fraction, name = None):
+    def __init__(self, graph, attack_node_fraction, name=None):
         self.graph = graph
         self.graph = dgl.add_self_loop(self.graph)
         self.dataset_name = name
@@ -48,6 +49,7 @@ class graph_to_dataset:
         self.train_mask = th.BoolTensor(self.graph.ndata['train_mask'])
         self.test_mask = th.BoolTensor(self.graph.ndata['test_mask'])
 
+
 class WatermarkGraph:
     def __init__(self, n, num_features, num_classes, pr=0.1, pg=0, device='cpu'):
         self.pr = pr
@@ -57,25 +59,31 @@ class WatermarkGraph:
 
     def _generate_wm(self, n, num_features, num_classes):
         wm_edge_index = erdos_renyi_graph(n, self.pg, directed=False)
-        wm_x = torch.tensor(np.random.binomial(1, self.pr, size=(n, num_features)), dtype=torch.float32)
-        wm_y = torch.tensor(np.random.randint(low=0, high=num_classes, size=n), dtype=torch.long)
-        
+        wm_x = torch.tensor(np.random.binomial(
+            1, self.pr, size=(n, num_features)), dtype=torch.float32)
+        wm_y = torch.tensor(np.random.randint(
+            low=0, high=num_classes, size=n), dtype=torch.long)
+
         data = dgl.graph((wm_edge_index[0], wm_edge_index[1]), num_nodes=n)
         data.ndata['feat'] = wm_x
         data.ndata['label'] = wm_y
         return data
 
+
 class GraphSAGE(nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
         super(GraphSAGE, self).__init__()
-        self.conv1 = SAGEConv(in_channels, hidden_channels, aggregator_type='mean')
-        self.conv2 = SAGEConv(hidden_channels, out_channels, aggregator_type='mean')
+        self.conv1 = SAGEConv(in_channels, hidden_channels,
+                              aggregator_type='mean')
+        self.conv2 = SAGEConv(
+            hidden_channels, out_channels, aggregator_type='mean')
 
     def forward(self, blocks, x):
         x = self.conv1(blocks[0], x)
         x = F.relu(x)
         x = self.conv2(blocks[1], x)
         return x
+
 
 class Defense:
     def __init__(self, dataset, attack_node_fraction):
@@ -105,14 +113,14 @@ class Defense:
         self.train_mask = dataset.train_mask
         self.test_mask = dataset.test_mask
 
-    def train(self,loader):
+    def train(self, loader):
         self.model.train()
         total_loss = 0
         for _, _, blocks in loader:
             blocks = [b.to(self.device) for b in blocks]
             input_features = blocks[0].srcdata['feat']
             output_labels = blocks[-1].dstdata['label']
-        
+
             self.optimizer.zero_grad()
             output_predictions = self.model(blocks, input_features)
             loss = F.cross_entropy(output_predictions, output_labels)
@@ -121,8 +129,8 @@ class Defense:
             total_loss += loss.item()
         return total_loss / len(loader)
 
-    # Testing function    
-    def test(self,loader):
+    # Testing function
+    def test(self, loader):
         self.model.eval()
         correct = 0
         total = 0
@@ -136,7 +144,7 @@ class Defense:
                 correct += (pred == output_labels).sum().item()
                 total += len(output_labels)
         return correct / total
-    
+
     def merge_cora_and_datawm(self, cora_graph, datawm):
         # Ensure both graphs are on the same device
         device = cora_graph.device
@@ -154,8 +162,10 @@ class Defense:
         if cora_feat_dim != wm_feat_dim:
             # Pad or truncate watermark features to match Cora features
             if cora_feat_dim > wm_feat_dim:
-                padding = torch.zeros(num_wm_nodes, cora_feat_dim - wm_feat_dim, device=device)
-                datawm.ndata['feat'] = torch.cat([datawm.ndata['feat'], padding], dim=1)
+                padding = torch.zeros(
+                    num_wm_nodes, cora_feat_dim - wm_feat_dim, device=device)
+                datawm.ndata['feat'] = torch.cat(
+                    [datawm.ndata['feat'], padding], dim=1)
             else:
                 datawm.ndata['feat'] = datawm.ndata['feat'][:, :cora_feat_dim]
 
@@ -164,20 +174,24 @@ class Defense:
             if key not in datawm.ndata:
                 if key in ['train_mask', 'val_mask', 'test_mask']:
                     # For mask attributes, initialize with False
-                    datawm.ndata[key] = torch.zeros(num_wm_nodes, dtype=torch.bool, device=device)
+                    datawm.ndata[key] = torch.zeros(
+                        num_wm_nodes, dtype=torch.bool, device=device)
                 elif key == 'norm':
                     # For 'norm', initialize with ones (assuming it's used for normalization)
-                    datawm.ndata[key] = torch.ones(num_wm_nodes, 1, dtype=torch.float32, device=device)
+                    datawm.ndata[key] = torch.ones(
+                        num_wm_nodes, 1, dtype=torch.float32, device=device)
                 else:
                     # For any other attributes, initialize with zeros
                     shape = (num_wm_nodes,) + cora_graph.ndata[key].shape[1:]
-                    datawm.ndata[key] = torch.zeros(shape, dtype=cora_graph.ndata[key].dtype, device=device)
+                    datawm.ndata[key] = torch.zeros(
+                        shape, dtype=cora_graph.ndata[key].dtype, device=device)
 
         # Merge the graphs
         merged_graph = dgl.batch([cora_graph, datawm])
 
         # Update watermark mask
-        wm_mask = torch.zeros(num_cora_nodes + num_wm_nodes, dtype=torch.bool, device=device)
+        wm_mask = torch.zeros(num_cora_nodes + num_wm_nodes,
+                              dtype=torch.bool, device=device)
         wm_mask[num_cora_nodes:] = True
         merged_graph.ndata['wm_mask'] = wm_mask
 
@@ -185,26 +199,27 @@ class Defense:
 
     def generate_extended_label_file(self, datasetCora_merge, original_node_count, new_node_count, output_file):
         labels = datasetCora_merge.labels
-        
+
         num_classes = len(torch.unique(datasetCora_merge.labels))
-        
+
         file_exists = os.path.isfile(output_file)
-        
+
         with open(output_file, 'w') as f:
-            
+
             for i in range(original_node_count):
                 f.write(f"{i} {labels[i]}\n")
-        
+
             import random
             for i in range(original_node_count, original_node_count + new_node_count):
                 new_label = random.randint(0, num_classes - 1)
                 f.write(f"{i} {new_label}\n")
 
     def watermark_attack(self, dataset, attack_name, dataset_name):
-        datasetCora = Watermark_sage(dataset,0.25)
+        datasetCora = Watermark_sage(dataset, 0.25)
         datasetCora.attack()
 
-        graph = datasetCora.merge_cora_and_datawm(datasetCora.graph, datasetCora.datawm)
+        graph = datasetCora.merge_cora_and_datawm(
+            datasetCora.graph, datasetCora.datawm)
         datasetCora_merge = graph_to_dataset(graph, 0.25, dataset.dataset_name)
 
         flag = False
@@ -214,10 +229,11 @@ class Defense:
                 attack.attack()
                 flag = True
             elif (attack_name == 2):
-                original_node_count = 2708  
-                new_node_count = 50  
+                original_node_count = 2708
+                new_node_count = 50
                 output_file = "./GNNIP/data/attack2_generated_graph/cora/query_labels_cora.txt"
-                self.generate_extended_label_file(datasetCora_merge, original_node_count, new_node_count, output_file)
+                self.generate_extended_label_file(
+                    datasetCora_merge, original_node_count, new_node_count, output_file)
                 attack = ModelExtractionAttack1(
                     datasetCora_merge, 0.25, "./GNNIP/data/attack2_generated_graph/cora/selected_index.txt",
                     "./GNNIP/data/attack2_generated_graph/cora/query_labels_cora.txt",
@@ -244,14 +260,15 @@ class Defense:
                 flag = True
         elif (dataset_name == 2):
             if (attack_name == 1):
-                attack = ModelExtractionAttack0(datasetCora_merge,0.25)
+                attack = ModelExtractionAttack0(datasetCora_merge, 0.25)
                 attack.attack()
                 flag = True
             elif (attack_name == 2):
-                original_node_count = 3327 
-                new_node_count = 50  
+                original_node_count = 3327
+                new_node_count = 50
                 output_file = "./GNNIP/data/attack2_generated_graph/citeseer/query_labels_citeseer.txt"
-                self.generate_extended_label_file(datasetCora_merge, original_node_count, new_node_count, output_file)
+                self.generate_extended_label_file(
+                    datasetCora_merge, original_node_count, new_node_count, output_file)
                 attack = ModelExtractionAttack1(
                     datasetCora_merge, 0.25, "./GNNIP/data/attack2_generated_graph/citeseer/selected_index.txt",
                     "./GNNIP/data/attack2_generated_graph/citeseer/query_labels_citeseer.txt",
@@ -259,11 +276,11 @@ class Defense:
                 attack.attack()
                 flag = True
             elif (attack_name == 3):
-                attack = ModelExtractionAttack2(datasetCora_merge,0.25)
+                attack = ModelExtractionAttack2(datasetCora_merge, 0.25)
                 attack.attack()
                 flag = True
             elif (attack_name == 4):
-                attack = ModelExtractionAttack3(datasetCora_merge,0.25)
+                attack = ModelExtractionAttack3(datasetCora_merge, 0.25)
                 attack.attack()
                 flag = True
             elif (attack_name == 5):
@@ -282,10 +299,11 @@ class Defense:
                 attack.attack()
                 flag = True
             elif (attack_name == 2):
-                original_node_count = 19717 
-                new_node_count = 50  
+                original_node_count = 19717
+                new_node_count = 50
                 output_file = "./GNNIP/data/attack2_generated_graph/pubmed/query_labels_pubmed.txt"
-                self.generate_extended_label_file(datasetCora_merge, original_node_count, new_node_count, output_file)
+                self.generate_extended_label_file(
+                    datasetCora_merge, original_node_count, new_node_count, output_file)
                 attack = ModelExtractionAttack1(
                     datasetCora_merge, 0.25, "./GNNIP/data/attack2_generated_graph/pubmed/selected_index.txt",
                     "./GNNIP/data/attack2_generated_graph/pubmed/query_labels_pubmed.txt",
@@ -312,13 +330,17 @@ class Defense:
                 attack.attack()
         if (flag == True):
             datawm = datasetCora.datawm
-            datasetCora_wm = graph_to_dataset(datawm,0.25, dataset.dataset_name)
-            datasetCora_wm.test_mask = torch.ones_like(datasetCora_wm.test_mask, dtype=torch.bool)
+            datasetCora_wm = graph_to_dataset(
+                datawm, 0.25, dataset.dataset_name)
+            datasetCora_wm.test_mask = torch.ones_like(
+                datasetCora_wm.test_mask, dtype=torch.bool)
             net = Gcn_Net(attack.feature_number, attack.label_number)
             evaluation = GraphNeuralNetworkMetric(
-                                0, 0, net, datasetCora_wm.graph, datasetCora_wm.features, datasetCora_wm.test_mask, datasetCora_wm.labels)
+                0, 0, net, datasetCora_wm.graph, datasetCora_wm.features, datasetCora_wm.test_mask, datasetCora_wm.labels)
             evaluation.evaluate()
             print("Watermark Graph - Accuracy:", evaluation.accuracy)
+
+
 class Watermark_sage(Defense):
     def __init__(self, dataset, attack_node_fraction, wm_node=50, pr=0.1, pg=0, device="cpu"):
         super().__init__(dataset, attack_node_fraction)
@@ -327,32 +349,35 @@ class Watermark_sage(Defense):
         self.pr = pr
         self.pg = pg
         self.device = device
-        
-        self.model = GraphSAGE(in_channels=self.graph.ndata['feat'].shape[1], hidden_channels=128, out_channels=7)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.01, weight_decay=5e-4)
 
-    def erdos_renyi_graph(self,n, p, directed=False):
+        self.model = GraphSAGE(
+            in_channels=self.graph.ndata['feat'].shape[1], hidden_channels=128, out_channels=7)
+        self.optimizer = torch.optim.Adam(
+            self.model.parameters(), lr=0.01, weight_decay=5e-4)
+
+    def erdos_renyi_graph(self, n, p, directed=False):
         edges = []
         for i in range(n):
-            for j in range(i+1, n): 
+            for j in range(i+1, n):
                 if torch.rand(1).item() < p:
                     edges.append([i, j])
                     if not directed:
                         edges.append([j, i])
-    
+
         if not edges:
             i, j = torch.randint(0, n, (2,))
             while i == j:
                 j = torch.randint(0, n, (1,))
             edges = [[i.item(), j.item()], [j.item(), i.item()]]
-    
+
         edges = torch.tensor(edges, dtype=torch.long).t()
         return edges
 
     def attack(self):
         # enable_cpu_affinity()
-        data_wm = WatermarkGraph(n=self.wm_node, num_features=self.feature_number, num_classes=self.label_number, pr=self.pr, pg=self.pg, device=self.device).graph_wm
-        
+        data_wm = WatermarkGraph(n=self.wm_node, num_features=self.feature_number,
+                                 num_classes=self.label_number, pr=self.pr, pg=self.pg, device=self.device).graph_wm
+
         self.datawm = data_wm
 
         # Choose devices
@@ -411,7 +436,7 @@ class Watermark_sage(Defense):
         # Ensure features and labels are on the correct device
         self.graph.ndata['feat'] = self.graph.ndata['feat'].to(self.device)
         self.graph.ndata['label'] = self.graph.ndata['label'].to(self.device)
-        
+
         # Training and evaluation
         for epoch in tqdm(range(1, 51)):
             loss = self.train(train_dataloader)
@@ -431,5 +456,5 @@ class Watermark_sage(Defense):
         marked_acc = self.test(test_dataloader)
         watermark_acc = self.test(wm_dataloader)
         print('Final results')
-        print(f'Non-Marked Acc: {nonmarked_acc:.4f}, Marked Acc: {marked_acc:.4f}, Watermark Acc: {watermark_acc:.4f}')
-
+        print('Non-Marked Acc: {:.4f}, Marked Acc: {:.4f}, Watermark Acc: {:.4f}'.format(
+            nonmarked_acc, marked_acc, watermark_acc))
--- examples/examples.py
+++ examples/examples.py
@@ -1,6 +1,7 @@
 from gnnip.datasets.datasets import *
-# from gnnip.datasets.datasets_backup import
 from gnnip.core_algo import *
+from gnnip.core_algo import *
+from gnnip.core_algo.Defense import Watermark_sage
 
 
 # dataset = Cora()
@@ -87,8 +88,22 @@ def run_attack(attack_type, dataset_name):
         print(a)
 
 
-dataset_name = input("Enter dataset name (Cora, Citeseer, PubMed): ")
-attack_type = int(input("Enter attack type (0-5): "))
+model_type = input("Enter model type (attack or defense): ").strip().lower()
 
-
-run_attack(attack_type, dataset_name)
+if model_type == "attack":
+    dataset_name = input("Enter dataset name (Cora, Citeseer, PubMed): ")
+    attack_type = int(input("Enter attack type (0-5): "))
+    run_attack(attack_type, dataset_name)
+elif model_type == "defense":
+    attack_name = int(input("Please choose the number:\n1.ModelExtractionAttack0\n2.ModelExtractionAttack1\n3.ModelExtractionAttack2\n4.ModelExtractionAttack3\n5.ModelExtractionAttack4\n6.ModelExtractionAttack5\n"))
+    dataset_name = int(
+        input("Please choose the number:\n1.Cora\n2.Citeseer\n3.PubMed\n"))
+    if (dataset_name == 1):
+        defense = Watermark_sage(Cora(), 0.25)
+        defense.watermark_attack(Cora(), attack_name, dataset_name)
+    elif (dataset_name == 2):
+        defense = Watermark_sage(Citeseer(), 0.25)
+        defense.watermark_attack(Citeseer(), attack_name, dataset_name)
+    elif (dataset_name == 3):
+        defense = Watermark_sage(PubMed(), 0.25)
+        defense.watermark_attack(PubMed(), attack_name, dataset_name)
