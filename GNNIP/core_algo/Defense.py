85b4f62e64bd54d116fbb896acf9ea8e0fe13103
Yuxiang <128219376+Sethanyx@users.noreply.github.com>
2024-08-24 01:07:21 -0500

    Update Defense.py

--- GNNIP/core_algo/Defense.py
+++ GNNIP/core_algo/Defense.py
@@ -433,15 +433,3 @@ class Watermark_sage(Defense):
         print('Final results')
         print(f'Non-Marked Acc: {nonmarked_acc:.4f}, Marked Acc: {marked_acc:.4f}, Watermark Acc: {watermark_acc:.4f}')
 
-attack_name = int(input("Please choose the number:\n1.ModelExtractionAttack0\n2.ModelExtractionAttack1\n3.ModelExtractionAttack2\n4.ModelExtractionAttack3\n5.ModelExtractionAttack4\n6.ModelExtractionAttack5\n"))
-dataset_name = int(input("Please choose the number:\n1.Cora\n2.Citeseer\n3.PubMed\n"))
-if (dataset_name == 1):
-    defense = Watermark_sage(Cora(),0.25)
-    defense.watermark_attack(Cora(), attack_name, dataset_name)
-elif (dataset_name == 2):
-    defense = Watermark_sage(Citeseer(),0.25)
-    defense.watermark_attack(Citeseer(), attack_name, dataset_name)
-elif (dataset_name == 3):
-    defense = Watermark_sage(PubMed(),0.25)
-    defense.watermark_attack(PubMed(), attack_name, dataset_name)
-
